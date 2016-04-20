659515648934bfe2ea2a8fcd50c7219b070b62f2
matthannam <mhannam@gmail.com>
2016-04-20 12:49:17 -0400

    Map URLS for donation widget

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,7 +9,9 @@
 	var firstAmount = G.amountGen(35, 5),
 			secAmount = G.amountGen(75, 10);
 
-	$('#random1').html('$'+ firstAmount);
-	$('#random2').html('$'+ secAmount);
+	$('.donation-val1').html('$'+ firstAmount);
+	$('.donation-val2').html('$'+ secAmount);
+	$('.donation-val1').attr('href', 'https://secure.actblue.com/contribute/page/rossfornc?refcode=website_splash&recurring=true&express_lane=true&reccuring=true&amount='+firstAmount);
+	$('.donation-val2').attr('href', 'https://secure.actblue.com/contribute/page/rossfornc?refcode=website_splash&recurring=true&express_lane=true&reccuring=true&amount='+secAmount);
 
 })(jQuery);
